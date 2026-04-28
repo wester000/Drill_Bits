d1fcc5cef8f542e5154e9e50b64f78b32c35fe99
veen.born@gmail.com <veen.born@gmail.com>
2026-04-28 15:07:43 +0000

    moved some stuff around

---
+++ python/database_manager.py
@@ -0,0 +1,75 @@
+import sqlite3
+from enum import Enum
+
+Clas
+
+def create_tables(cursor):
+# 1. Table for Single-Ended Screwdriver Bits
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS screw_bits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        bit_type TEXT NOT NULL,
+        size TEXT NOT NULL,
+        quantity INTEGER DEFAULT 1,
+        condition TEXT
+    )
+    ''')
+
+    # 2. Table for Drill Bits
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS drill_bits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        material TEXT NOT NULL,
+        size TEXT NOT NULL,
+        quantity INTEGER DEFAULT 1,
+        condition TEXT
+    )
+    ''')
+
+    # 3. Table for Double-Ended Bits
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS double_ended_bits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        end1_type TEXT NOT NULL,
+        end1_size TEXT NOT NULL,
+        end2_type TEXT NOT NULL,
+        end2_size TEXT NOT NULL,
+        quantity INTEGER DEFAULT 1,
+        condition TEXT
+    )
+    ''')
+    return
+
+def drop_tables(cursor):
+    # This deletes the table structure and all data within them
+    cursor.execute("DROP TABLE IF EXISTS screw_bits")
+    cursor.execute("DROP TABLE IF EXISTS drill_bits")
+    cursor.execute("DROP TABLE IF EXISTS double_ended_bits")
+
+def empty_tables(cursor):
+    # This removes all rows but keeps the structure
+    cursor.execute("DELETE FROM screw_bits")
+    cursor.execute("DELETE FROM drill_bits")
+    cursor.execute("DELETE FROM double_ended_bits")
+        
+    cursor.execute("DELETE FROM sqlite_sequence WHERE name IN ('screw_bits', 'drill_bits', 'double_ended_bits')")
+
+
+def insert_bit(cursor, bit_type, type, size, )
+
+
+
+
+
+def main():
+    conn = sqlite3.connect('workshop_inventory.db')
+    cursor = conn.cursor()
+    create_tables(cursor)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+
+# Connect to the database
